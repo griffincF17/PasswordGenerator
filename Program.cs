856f2950820b98b2071919b961f771ca34c0d1d6
Cody Griffin <griffincody38@yahoo.com>
2018-03-06 12:58:29 -0500

    Updated the C# file
    The updated C# file includes comments to make the code easier to read.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,10 @@ namespace PassGen
 {
     public class Password
     {
+        //Member variable(s)
         private int len;
+        
+        //Password function that returns the password string
         public string password()
         {
             var chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%?&*()+-";
@@ -22,10 +25,14 @@ namespace PassGen
             }
             return p;
         }
+        
+        //Constructor
         public Password(int l)
         {
             Len = l;
         }
+        
+        //Verifies the length variable to determine if the length is greater than or equal to 0.
         public int Len
         {
             get
@@ -49,21 +56,31 @@ namespace PassGen
     {
         static void Main(string[] args)
         {
-            
+            //Try-catch block to catch invalid input
             try
             {
+                //Variable(s)
                 int length;
+                
+                //Prompts user for the password length
                 Console.Write("Enter the password length: ");
                 length = Convert.ToInt32(Console.ReadLine());
-
+                
+                //Creates Password object
                 Password p = new Password(length);
+               
+                //Prints the generated password
                 Console.WriteLine("Generated password: {0}", p.password());
             }
+            
+            //Catches the case where the length is negative
             catch(ArgumentOutOfRangeException ex)
             {
                 Console.WriteLine("Exception during execution: ");
                 Console.WriteLine(ex.Message);
             }
+            
+            //Catches the case where the length is not an integer
             catch(FormatException fe)
             {
                 Console.WriteLine("Exception during execution: ");
